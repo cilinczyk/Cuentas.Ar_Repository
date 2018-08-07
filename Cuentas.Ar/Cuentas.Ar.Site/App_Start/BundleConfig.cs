c95fc91a141da6045622e07f8d2e005813624eae
Cristian <cristian_ilinczyk@hotmail.com>
2018-08-06 21:16:25 -0300

    Configuracion de bootstrap.

--- Cuentas.Ar/Cuentas.Ar.Site/App_Start/BundleConfig.cs
+++ Cuentas.Ar/Cuentas.Ar.Site/App_Start/BundleConfig.cs
@@ -1,30 +1,44 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace Cuentas.Ar.Site
 {
     public class BundleConfig
     {
-        // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*"));
 
-            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
-            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //// Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
+            //// para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                "~/Content/vendor/bootstrap/css/bootstrap.min.css",
+                "~/Content/vendor/metisMenu/metisMenu.min.css",
+                "~/Content/dist/css/sb-admin-2.css",
+                "~/Content/vendor/morrisjs/morris.css",
+                "~/Content/vendor/font-awesome/css/font-awesome.min.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                "~/Content/vendor/jquery/jquery.min.js",
+                "~/Content/vendor/bootstrap/js/bootstrap.min.js",
+                "~/Content/vendor/metisMenu/metisMenu.min.js",
+                "~/Content/vendor/raphael/raphael.min.js",
+                "~/Content/vendor/morrisjs/morris.min.js",
+                "~/Content/data/morris-data.js",
+                "~/Content/dist/js/sb-admin-2.js"));
         }
     }
 }
