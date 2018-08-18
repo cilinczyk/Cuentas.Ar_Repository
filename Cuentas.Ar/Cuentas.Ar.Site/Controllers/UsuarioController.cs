737a1fe5797b1008f304d4b883325133eed44fe1
Cristian <cristian_ilinczyk@hotmail.com>
2018-08-18 00:44:54 -0300

    Fixes Login.

---
+++ Cuentas.Ar/Cuentas.Ar.Site/Controllers/UsuarioController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using Cuentas.Ar.Business;
+using Cuentas.Ar.Entities;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+namespace Cuentas.Ar.Site.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult AltaLogin()
+        {
+            var usuario = new Usuario();
+            usuario.Administrador = false;
+
+            return View();
+        }
+    }
+}
