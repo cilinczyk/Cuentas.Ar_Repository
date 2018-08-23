2361210c4923c5ff062fa6e7ebc33765aa2294e6
Cristian <cristian_ilinczyk@hotmail.com>
2018-08-23 17:57:13 -0300

    Fixes del login y crear cuenta.

--- Cuentas.Ar/Cuentas.Ar.Site/App_Start/BundleConfig.cs
+++ Cuentas.Ar/Cuentas.Ar.Site/App_Start/BundleConfig.cs
@@ -9,19 +9,14 @@ namespace Cuentas.Ar.Site
             bundles.Add(new StyleBundle("~/bundles/css").Include(
                 "~/Content/CuentasArStyleSheet.css",
                 "~/Content/vendor/bootstrap/css/bootstrap.min.css",
-                "~/Content/vendor/metisMenu/metisMenu.min.css",
                 "~/Content/dist/css/sb-admin-2.css",
-                "~/Content/vendor/morrisjs/morris.css",
+                "~/Content/vendor/metisMenu/metisMenu.min.css",
                 "~/Content/vendor/font-awesome/css/font-awesome.min.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
-                "~/Content/vendor/jquery/jquery.min.js",
-                "~/Content/vendor/bootstrap/js/bootstrap.min.js",
-                "~/Content/vendor/metisMenu/metisMenu.min.js",
-                "~/Content/vendor/raphael/raphael.min.js",
-                "~/Content/vendor/morrisjs/morris.min.js",
-                "~/Content/data/morris-data.js",
-                "~/Content/dist/js/sb-admin-2.js"));
+                "~/Scripts/jquery-3.3.1.min.js",
+                "~/Content/dist/js/sb-admin-2.js",
+                "~/Content/vendor/metisMenu/metisMenu.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
             "~/Scripts/jquery.validate*",
