497bf11b7af2def10781eaa56da60a390792ae9f
Cristian <cristian_ilinczyk@hotmail.com>
2018-10-29 14:37:21 -0300

    Fixes ABM de Categoria.

---
+++ Cuentas.Ar/Cuentas.Ar.Entities/Entidades/Categoria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Foolproof;
+
+namespace Cuentas.Ar.Entities
+{
+    [MetadataType(typeof(CategoriaMetadata))]
+    public partial class Categoria
+    {
+    }
+
+    public class CategoriaMetadata
+    {
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Debe seleccionar un tipo de registro.")]
+        public int idTipoRegistro { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Debe ingresar una categoría.")]
+        [StringLength(50, ErrorMessage = "La categoría no debe tener un maximo de 50 caracteres.")]
+        public string Descripcion { get; set; }
+    }
+}
