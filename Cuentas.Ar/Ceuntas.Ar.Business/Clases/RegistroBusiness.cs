5d7528d5b3c99e02c730ed7bd66ce73a08caa13d
Cristian <cristian_ilinczyk@hotmail.com>
2018-11-09 09:55:26 -0300

    Avances en el modulo de registros.

---
+++ Cuentas.Ar/Ceuntas.Ar.Business/Clases/RegistroBusiness.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Cuentas.Ar.Entities;
+using Cuentas.Ar.Repository;
+
+namespace Cuentas.Ar.Business
+{
+    public class RegistroBusiness
+    {
+        private readonly RegistroRepository repositorio;
+
+        public RegistroBusiness()
+        {
+            this.repositorio = new RegistroRepository();
+        }
+
+        public List<Registro> Listar()
+        {
+            return repositorio.Listar();
+        }
+
+        public Registro Obtener(int idRegistro)
+        {
+            return repositorio.Obtener(idRegistro);
+        }
+
+        public Registro ObtenerCompleto(int idRegistro)
+        {
+            return repositorio.ObtenerCompleto(idRegistro);
+        }
+
+        public int Guardar(Registro model)
+        {
+            return repositorio.Guardar(model);
+        }
+
+        public int Modificar(Registro model)
+        {
+            return repositorio.Modificar(model);
+        }
+
+        public void Eliminar(int idRegistro)
+        {
+            repositorio.Eliminar(idRegistro);
+        }
+    }
+}
---
+++ Cuentas.Ar/Cuentas.Ar.Repository/Clases/RegistroRepository.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Cuentas.Ar.Entities;
+
+namespace Cuentas.Ar.Repository
+{
+    public class RegistroRepository
+    {
+        public List<Registro> Listar()
+        {
+            using (var context = new CuentasArEntities())
+            {
+                return context.Registro.Include("TipoRegistro").Include("Categoria").Include("SubCategoria").Include("Moneda").OrderBy(x => x.Fecha).ToList();
+            }
+        }
+
+        public Registro Obtener(int idRegistro)
+        {
+            using (var context = new CuentasArEntities())
+            {
+                return context.Registro.FirstOrDefault(x => x.idRegistro == idRegistro);
+            }
+        }
+
+        public Registro ObtenerCompleto(int idRegistro)
+        {
+            using (var context = new CuentasArEntities())
+            {
+                return context.Registro.Include("TipoRegistro").Include("Categoria").Include("SubCategoria").Include("Moneda").FirstOrDefault(x => x.idRegistro == idRegistro);
+            }
+        }
+
+        public int Guardar(Registro model)
+        {
+            try
+            {
+                using (var context = new CuentasArEntities())
+                {
+                    context.Registro.Add(model);
+                    context.SaveChanges();
+                }
+
+                return model.idRegistro;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("No se puede guardar el registro.", ex);
+            }
+        }
+
+        public int Modificar(Registro model)
+        {
+            try
+            {
+                using (var context = new CuentasArEntities())
+                {
+                    context.Registro.Attach(model);
+                    context.Entry(model).Property(x => x.idTipoRegistro).IsModified = true;
+                    context.Entry(model).Property(x => x.idCategoria).IsModified = true;
+                    context.Entry(model).Property(x => x.idSubCategoria).IsModified = true;
+                    context.Entry(model).Property(x => x.idMoneda).IsModified = true;
+                    context.Entry(model).Property(x => x.Importe).IsModified = true;
+                    context.Entry(model).Property(x => x.Observaciones).IsModified = true;
+                    context.Entry(model).Property(x => x.Fecha).IsModified = true;
+
+                    context.SaveChanges();
+                }
+
+                return model.idRegistro;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("No se puede modificar el registro.", ex);
+            }
+        }
+
+        public void Eliminar(int idRegistro)
+        {
+            try
+            {
+                using (var context = new CuentasArEntities())
+                {
+                    context.Entry(this.Obtener(idRegistro)).State = EntityState.Deleted;
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("No se puede eliminar el registro.", ex);
+            }
+        }
+    }
+}
