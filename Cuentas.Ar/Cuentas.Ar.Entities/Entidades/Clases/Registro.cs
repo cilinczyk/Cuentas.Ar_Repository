59c0730a1d6517ebff2cfaf87e7bcc2246b1f50b
Cristian <cristian_ilinczyk@hotmail.com>
2018-11-13 15:10:28 -0300

    Nuevos puntos de menú y validaciones.

--- Cuentas.Ar/Cuentas.Ar.Entities/Entidades/Clases/Registro.cs
+++ Cuentas.Ar/Cuentas.Ar.Entities/Entidades/Clases/Registro.cs
@@ -15,7 +15,19 @@ namespace Cuentas.Ar.Entities
         [Required(AllowEmptyStrings = false, ErrorMessage = "Debe seleccionar un tipo de registro.")]
         public int idTipoRegistro { get; set; }
 
-        [StringLength(50, ErrorMessage = "La categoría no debe tener un maximo de 50 caracteres.")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Debe seleccionar una categoría.")]
+        public int idCategoria { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Debe seleccionar una moneda.")]
+        public int idMoneda { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Debe ingresar un importe.")]
+        public decimal Importe { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Debe ingresar una fecha.")]
+        public System.DateTime Fecha { get; set; }
+
+        [StringLength(50, ErrorMessage = "La descripción debe tener un maximo de 50 caracteres.")]
         public string Descripcion { get; set; }
     }
 }
